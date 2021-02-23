6b0122f7339dd677177096ab7dcad6620e802ad5
Constantin <62800068+consuatin@users.noreply.github.com>
2021-02-23 03:24:28 +0100

    fixed the space

--- script.js
+++ script.js
@@ -1,51 +1,37 @@
-const gridContainer = document.querySelector("#container");
+const gridContainer = document.querySelector("#container")
 
-gridStart(16);
-
-let randomColor = () => {
-    let colorsList = ["rgb(54, 100, 248)",
-                    "rgb(250, 97, 69)",
-                    "rgb(56, 247, 230)",
-                    "rgb(44, 176, 204)",
-                    "rgb(27, 23, 78)",
-                    "rgb(137, 143, 17)reen",
-                    "rgb(164, 48, 82)",
-                    "rgb(137, 104, 173)",
-                    "rgb(17, 122, 170)",
-                    "rgb(141, 152, 170)",
-                    "rgb(159, 3, 1)"]
-    randomNum = Math.floor(Math.random()*colorsList.length);
-    result = colorsList[randomNum];
-    return result;
+function randomColor() {
+   return `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
 }
 
 function gridStart(gridSize) {
-    let grids = gridSize**2;
-    for (let i=0; i<(grids); i++) {
-        const gridElement = document.createElement("div");
-        gridElement.classList.add('grid');
-        gridElement.style.height = (400/gridSize-2)+"px";
-        gridElement.style.width = (400/gridSize-2)+"px";
-        gridContainer.appendChild(gridElement);
-    
-        function changeColor() {
-            gridElement.style.background = randomColor();
-        }
-        gridElement.addEventListener("mouseover", changeColor);
+    while (gridContainer.firstChild) gridContainer.firstChild.remove()
+
+    for (let i = 0, grids = gridSize ** 2; i < grids; i++) {
+        const gridElement = document.createElement("div")
+        gridElement.classList.add('grid')
+        gridElement.style.height = `${100/gridSize}%`
+        gridElement.style.width = `${100/gridSize}%`
+        gridContainer.appendChild(gridElement)
+        gridElement.addEventListener("mouseover", event => {
+            event.target.style.background = randomColor()
+        })
     }
 }
 
 function changeSize() {
-    let newSize = prompt("Type a number between 1-64");
-    gridContainer.innerHTML="";
-    if ( newSize<=0 || newSize >64) {
-        alert("wrong number");
-        changeSize();
-    } else {
-        gridStart(newSize);
+    const min = 1, max = 64
+    let newSize = prompt(`Type a number between ${min} - ${max}`)
+    if (newSize) {
+        if (newSize >= min && newSize <= max) {
+            gridStart(newSize)
+        } else {
+            alert("wrong number")
+            changeSize()
+        }
     }
 }
 
-
-
-
+window.addEventListener('load', () => {
+    gridStart(16)
+})
